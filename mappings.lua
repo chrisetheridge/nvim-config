7a1b65800fd9c4db4a762efd22af4237c923eaee
Chris Etheridge <chrise@cognician.com>
2023-04-02 17:38:51 +0200

    more mappings

--- mappings.lua
+++ mappings.lua
@@ -3,15 +3,15 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+    [";"] = {
+      ":",
+      "enter command mode",
+      opts = {
+        nowait = true
+      }
+    }
+  }
 }
-
--- l = {
---   -- name = "LSP",
--- },
--- -- R = { name = "REST" },
-
 
 -- more keybinds!
 M.custom = {
@@ -22,7 +22,7 @@ M.custom = {
     ["<leader>wa"] = { vim.lsp.buf.add_workspace_folder, "Workspace Add Folder" },
     ["<leader>wr"] = { vim.lsp.buf.remove_workspace_folder, "Workspace Remove Folder" },
     -- Oil
-    ["<leader>oi"] = { ":Oil .<cr>", "Oil Dir Editor", },
+    ["<leader>oi"] = { ":Oil .<cr>", "Oil Dir Editor" },
     -- Harpoon
     ["<leader>ba"] = { function()
       require("harpoon.mark").add_file()
@@ -36,71 +36,47 @@ M.custom = {
     ["<leader>bh"] = { function()
       require("harpoon.ui").nav_prev()
     end, "Prev File" },
-  },
-  -- },
-  -- z = {
-  --   -- name = "Gitsigns",
-  --   b = { ":Gitsigns blame_line<cr>", "Blame line" },
-  --   p = { ":Gitsigns preview_hunk<cr>", "Preview hunk" },
-  --   s = { ":Gitsigns stage_hunk<cr>", "Stage hunk" },
-  --   u = { ":Gitsigns undo_stage_hunk<cr>", "Undo stage hunk" },
-  --   r = { ":Gitsigns reset_hunk<CR>", "Reset hunk" },
-  --   S = { ":Gitsigns stage_buffer<cr>", "Stage buffer" },
-  --   R = { ":Gitsigns reset_buffer<cr>", "Reset buffer" },
-  --   n = { ":Gitsigns next_hunk<cr>", "Next hunk" },
-  --   N = { ":Gitsigns prev_hunk<cr>", "Prev hunk" },
-  --   t = {
-  --     -- name = "Toggle",
-  --     b = { ":Gitsigns toggle_current_line_blame<cr>", "Curent line blame" },
-  --     d = { ":Gitsigns toggle_deleted<cr>", "Deleted" },
-  --   },
-  --   d = { ":Gitsigns diffthist<cr>", "Diff history" },
-  --   D = { ":Gitsigns diffhis ~<cr>", "Diff ~history" },
-  -- },
-  -- U = { ":UndotreeToggle<cr>", "Undo Tree" },
-
-  -- t = {
-  --   -- name = "Terminal",
-  --   v = { ":vsplit | term<cr>", "Vert Split Termial" },
-  --   s = { ":split | term<cr>", "Split Terminal" },
-  -- },
-  -- x = {
-  --   -- name = "Git",
-  --   g = { ":Neogit<cr>", "Status" },
-  --   p = { ":Git push<cr>", "Push" },
-  --   P = { ":Git push --force<cr>", "Force Push" },
-  --   f = { ":Git pull<cr>", "Pull" },
-  --   b = { ":Telescope git_branches<cr>", "Branches" },
-  --   -- b = { ":FzfLua git_branches<cr>", "Branches" },
-  -- },
-  -- e = { ":Neotree toggle<cr>", "Toggle Neo Tree" },
-  -- E = { ":Neotree focus<cr>", "Focus Neo Tree" },
-  -- s = { ":write<cr>", "Save buffer" },
-  -- S = { ":wa<cr>", "Save all" },
-  -- -- x = { save_all_quit, "Save and quit" },
-  -- q = { "<c-w>q", "Close window" },
-  -- r = { ":Telescope resume<cr>", "Resume last search" },
-  -- -- r = { ":FzfLua resume<cr>", "Resume last search" },
-  -- f = {
-  --   -- name = "Find",
-  --   F = { ":FzfLua<cr>", "Fzf" },
-  --   -- f = { ":FzfLua files<cr>", "Files" },
-  --   -- G = { ":FzfLua git_files<cr>", "Git Files" },
-  --   -- c = { ":FzfLua grep_curbuf<cr>", "In buffer" },
-  --   -- g = { ":FzfLua live_grep<cr>", "Live grep" },
-  --   -- b = { ":FzfLua buffers<cr>", "Buffers" },
-  --   -- k = { ":FzfLua keymaps<cr>", "Keymaps" },
-  --   -- r = { ":FzfLua oldfiles cwd_only=true<cr>", "Recent files" },
-
-  --   f = { ":Telescope find_files<cr>", "Files" },
-  --   c = { ":Telescope current_buffer_fuzzy_find<cr>", "In buffer" },
-  --   g = { ":Telescope live_grep<cr>", "Live grep" },
-  --   b = { ":Telescope buffers<cr>", "Buffers" },
-  --   k = { ":Telescope keymaps<cr>", "Keymaps" },
-  --   r = { ":Telescope oldfiles cwd_only=true<cr>", "Recent files" },
-  -- },
-  -- L = { ":Lazy<cr>", "Lazy" },
-  -- m = { ":Mason<cr>", "Mason" },
+    -- Gitsigns
+    ["<leader>zb"] = { ":Gitsigns blame_line<cr>", "Blame line" },
+    ["<leader>zp"] = { ":Gitsigns preview_hunk<cr>", "Preview hunk" },
+    ["<leader>zs"] = { ":Gitsigns stage_hunk<cr>", "Stage hunk" },
+    ["<leader>zu"] = { ":Gitsigns undo_stage_hunk<cr>", "Undo stage hunk" },
+    ["<leader>zr"] = { ":Gitsigns reset_hunk<CR>", "Reset hunk" },
+    ["<leader>zS"] = { ":Gitsigns stage_buffer<cr>", "Stage buffer" },
+    ["<leader>zR"] = { ":Gitsigns reset_buffer<cr>", "Reset buffer" },
+    ["<leader>zn"] = { ":Gitsigns next_hunk<cr>", "Next hunk" },
+    ["<leader>zN"] = { ":Gitsigns prev_hunk<cr>", "Prev hunk" },
+    ["<leader>ztb"] = { ":Gitsigns toggle_current_line_blame<cr>", "Curent line blame" },
+    ["<leader>ztd"] = { ":Gitsigns toggle_deleted<cr>", "Deleted" },
+    -- Undo tree 
+    ["<leader>U"] = { ":UndotreeToggle<cr>", "Undo Tree" },
+    -- Term
+    ["<leader>tv"] = { ":vsplit | term<cr>", "Vert Split Termial" }, 
+    ["<leader>ts"] = { ":split | term<cr>", "Split Terminal" }, 
+    -- Neogit / git
+    ["<leader>gg"] = { ":Neogit<cr>", "Status" },
+    ["<leader>gp"] = { ":Git push<cr>", "Push" }, 
+    ["<leader>gP"] = { ":Git push --force<cr>", "Force Push" }, 
+    ["<leader>gf"] = { ":Git pull<cr>", "Pull" },
+    ["<leader>gb"] = { ":Telescope git_branches<cr>", "Branches" },
+    -- Neotree
+    ["<leader>e"] = { ":Neotree toggle<cr>", "Toggle Neo Tree" },
+    ["<leader>E"] = { ":Neotree focus<cr>", "Focus Neo Tree" },
+    -- Writing
+    ["<leader>s"] = { ":write<cr>", "Save buffer" },
+    ["<leader>S"] = { ":wa<cr>", "Save all" },
+    -- Windows
+    ["<leader>q"] = { "<c-w>q", "Close window" },
+    -- Searching 
+    ["<leader>r"] = { ":Telescope resume<cr>", "Resume last search" },
+    -- Telescope
+    ["<leader>ff"] = { ":Telescope find_files<cr>", "Files" },
+    ["<leader>fc"] = { ":Telescope current_buffer_fuzzy_find<cr>", "In buffer" },
+    ["<leader>fg"] = { ":Telescope live_grep<cr>", "Live grep" },
+    ["<leader>fb"] = { ":Telescope buffers<cr>", "Buffers" },
+    ["<leader>fk"] = { ":Telescope keymaps<cr>", "Keymaps" },
+    ["<leader>fr"] = { ":Telescope oldfiles cwd_only=true<cr>", "Recent files" },
+  }
 }
 
 return M
